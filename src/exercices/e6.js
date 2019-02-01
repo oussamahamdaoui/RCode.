c1d83b30c5d1e9fe16719963740bd12b6a916a68
oussama hamdaoui <oussamahamdaoui@live.fr>
2019-02-01 22:14:02 +0100

    smal fixes

--- src/exercices/e6.js
+++ src/exercices/e6.js
@@ -2,19 +2,18 @@ const better = require('./../toBetterExercice');
 
 module.exports = better({
   expexted: {
-    max: 38,
-    table: [1, 9, 8, 13, 19, 27, 38, 6, 22],
+    i: 6,
   },
-  startWith: 'const table = [1,9,8,13,19,27,38,6,22];',
+  startWith: 'const table = [1,9,8,13,19,27,-1,38,6,22];',
   text: {
     fr: {
-      title: 'Parcourir un tableau',
-      text: 'Trouver la valeur max dans le tableau et la stoker dans une variable max',
+      title: 'Parcourir un tableau 2',
+      text: 'Trouver l\'index i de la valeur minimale',
     },
 
     en: {
-      title: 'Iterate over arrays',
-      text: 'Find the max value of table table and store it in a variable called max',
+      title: 'Iterate over arrays 2',
+      text: 'Find the index of the minimal value of table and store it in a variable called i',
     },
   },
 });
