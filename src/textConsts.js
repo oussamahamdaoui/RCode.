bc6553ff1799a97a3cf0549f5e67b9307897730c
Oussama Hamdaoui <oussama.hamdaoui@axa.fr>
2019-02-13 14:16:00 +0100

    remove french test

--- src/textConsts.js
+++ src/textConsts.js
@@ -1,7 +1,6 @@
 const Lang = require('./Lang');
 
 const USE_ONLY = new Lang({
-  fr: maxChars => `Utiliser seulment ${maxChars} characteres`,
   en: maxChars => `Use only ${maxChars} characters`,
 });
 
